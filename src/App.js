e35b41ce5881064377cd8b9130a6d96691d1595f
Cheng Ma <machengbeihang@gmail.com>
2020-08-13 13:26:55 +0800

    col title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const locale = {
+  en: {
+    id: 'ID',
+    name: 'Name',
+    office: 'Office',
+    phone: 'Phone', //
+    cellphone: 'Cell',
+    officephone: 'Office',
+  },
+  zh: {
+    id: '编号',
+    name: '姓名',
+    office: '办公室',
+    phone: '电话', //
+    cellphone: '手机',
+    officephone: '办公',
+  }
+}
+
+const colDef = [
+  { title: 'id' },
+  { title: 'name' },
+  {
+    title: 'phone',
+    subcol: [
+      { title: 'office' },
+      { title: 'cell' },
+    ]
+  },
+];
+
+class App extends Component {
+  state = {
+    records: [],
+    lan: 'zh',
+  }
+  inc(v) {
+    this.setState({
+      target: this.state.target + v
+    })
+  }
+
+  renderCols(coldef) {
+    return (
+      <div className='cols'>
+        { coldef.map(col => {
+          if (col.subcol) {
+            return (
+              <div>
+                <div className="col">
+                  { col.title }
+                </div>
+                { this.renderCols(col.subcol) }
+              </div>
+            )
+          } else {
+            return (
+              <div className='col'>{ col.title }</div>
+            )
+          }
+        }) }
+      </div>
+    )
+  }
+  render() {
+    return (
+      <div className='bookwrap'>
+        { this.renderCols(colDef) }
+      </div>
+    )
+  }
 }
 
 export default App;
